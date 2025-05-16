1907f9432309bc3a825eabb922d1e9cdecae514b
Freddie-789 <averageuser@mail.ru>
2025-05-16 23:02:24 +0700

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -57,8 +57,27 @@ grammar_verbs = pd.DataFrame({
         '%verb(fut)': results['%verb(fut)']
         })
 
+def get_result(neut, femn, masc, nouns, sg, pl, pres, past, future, verbs, file):
+    results['text'].append(file)
+    results['nouns'].append(nouns)
+    #results['neut'].append(neut)
+    results['%neut'].append(round(100 / nouns * neut if nouns > 0 else 0, 2))
+    #results['femn'].append(femn)
+    results['%femn'].append(round(100 / nouns * femn if nouns > 0 else 0, 2))
+    #results['masc'].append(masc)
+    results['%masc'].append(round(100 / nouns * masc if nouns > 0 else 0, 2))
+    results['%sing'].append(round(100 / nouns * sg if nouns > 0 else 0, 2))
+    results['%plur'].append(round(100 / nouns * pl if nouns > 0 else 0, 2))
+    results['verbs'].append(verbs)
+    # results['verb(pres)'].append(pres)
+    results['%verb(pres)'].append(round(100 / verbs * pres if verbs > 0 else 0, 2))
+    # results['verb(past)'].append(past)
+    results['%verb(past)'].append(round(100 / verbs * past if verbs > 0 else 0, 2))
+    # results['verb(fut)'].append(future)
+    results['%verb(fut)'].append(round(100 / verbs * future if verbs > 0 else 0, 2))
 
-for file_name in files:
+# Получаем статистику для первых 10 файлов (можно изменить)
+for num, file_name in enumerate(files[:10]):
     # Открываем файл в бинарном режиме только для чтения
     with open(directory + '/' + file_name, 'rb') as file:
         # Извлекаем текст в виде строки из pdf-файла
@@ -71,4 +90,8 @@ for file_name in files:
         text_UD = get_UDs(clear_text)
         # Выводим результат для каждого предложения
         print(readability(clear_text), impersonal_sentences(text_UD), compound_complex(text_UD), introductory_words(text_UD), participal_phrases(text_UD))
-        
+        neut, femn, masc, nouns, sg, pl, pres, past, future, verbs = get_grammar(text)
+        get_result(neut, femn, masc, nouns, sg, pl, pres, past, future, verbs, "scientific text " + str(num))
+
+print(grammar_nouns)
+print(grammar_verbs)

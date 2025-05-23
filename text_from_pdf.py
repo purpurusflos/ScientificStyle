a9c5154222db2a7532728645000977d23ab80ccb
Ris Mazhnikova <m.mazhnikova@g.nsu.ru>
2025-05-23 21:19:14 +0700

    Добавлены комментарии

--- text_from_pdf.py
+++ text_from_pdf.py
@@ -2,30 +2,27 @@ from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar
 
 
+# Извлекаем текст из указанного элемента.
 def text_extraction(element):
-    # Извлекаем текст из вложенного текстового элемента
     line_text = element.get_text()
 
-    # Находим форматы текста
-    # Инициализируем список со всеми форматами, встречающимися в строке текста
+    # Находим форматы текста.
+    # Инициализируем список со всеми форматами, встречающимися в строке текста.
     line_formats = []
     for text_line in element:
         if isinstance(text_line, LTTextContainer):
-            # Обходим каждый символ в строке текста
             for character in text_line:
                 if isinstance(character, LTChar):
-                    # Добавляем к символу название шрифта
+                    # Добавляем в список название и размер шрифта для каждого символа строки.
                     line_formats.append(character.fontname)
-                    # Добавляем к символу размер шрифта
                     line_formats.append(character.size)
-    # Находим уникальные размеры и названия шрифтов в строке
+    # Находим уникальные размеры и названия шрифтов в строке.
     format_per_line = list(set(line_formats))
 
-    # Возвращаем кортеж с текстом в каждой строке вместе с его форматом
+    # Возвращаем кортеж с текстом в каждой строке вместе с его форматом.
     return line_text, format_per_line
 
-# Очищаем текст от знаков переноса на новую строку и
-# объединяем в одну строку
+# Очищаем текст от знаков переноса на новую строку и объединяем в одну строку.
 def clean_text(page_content):
     text1 = ""
     text2 = ""
@@ -36,26 +33,31 @@ def clean_text(page_content):
     split_text2 = text1.split("\n")
     clean_text2 = "".join(split_text2)
     text2 += clean_text2
+    # Возвращает строку с текстом
     return text2
 
+
+# Получаем текст основной части статьи из pdf-файла
 def get_text(file_path):
     page_content = []
 
     for page_number, page in enumerate(extract_pages(file_path)):
-        # Находим все элементы
+        # Находим все элементы на странице.
         page_elements = page._objs
 
-        # Находим элементы, составляющие страницу
+        # Находим элементы, составляющие страницу.
         for i, element in enumerate(page_elements):
-            # Проверяем, является ли элемент текстовым
+            # Проверяем, является ли элемент текстовым.
             if isinstance(element, LTTextContainer):
-                # Используем функцию извлечения текста и формата для каждого текстового элемента
+                # Используем функцию извлечения текста и формата для каждого текстового элемента.
                 (line_text, format_per_line) = text_extraction(element)
-                # print(line_text, format_per_line)
-
+                # Проверяем, что текст относится именно к основной части
+                # и не является частью заголовков и других надписей
                 if 10.019999999999982 in format_per_line or 10.980000000000018 in format_per_line or 11.0 in format_per_line and len(line_text.split()) > 3:
                     page_content.append(line_text)
 
+                # Извлечение текста заканчивается, если в строке есть "Список литературы".
                 if "Список литературы" in line_text:
                     return clean_text(page_content)
+    # Возвращает строку с текстом основной части статьи без списка литературы и знаков переноса на другую строку.
     return clean_text(page_content)
